e56aed94b834aee4ff81429d04052b5f69fe3f1f
ifayost <ifayost@gmail.com>
2020-07-07 10:57:28 +0200

    Added AdamWarmup

--- transformers.py
+++ transformers.py
@@ -132,4 +132,25 @@ class Transformer(nn.Module):
         return out
 
 
-
+class AdamWarmup:
+    
+    def __init__(self, model_size, warmup_steps, optimizer):
+        
+        self.model_size = model_size
+        self.warmup_steps = warmup_steps
+        self.optimizer = optimizer
+        self.current_step = 0
+        self.lr = 0
+        
+    def get_lr(self):
+        return self.model_size ** (-0.5) * min(self.current_step ** (-0.5), self.current_step * self.warmup_steps ** (-1.5))
+        
+    def step(self):
+        # Increment the number of steps each time we call the step function
+        self.current_step += 1
+        lr = self.get_lr()
+        for param_group in self.optimizer.param_groups:
+            param_group['lr'] = lr
+        # update the learning rate
+        self.lr = lr
+        self.optimizer.step()   
